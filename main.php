fd35e00a4c03d81a27ae90c61c68152b5b324fed
Samiur Rahman <srsami707@gmail.com>
2023-10-08 12:05:31 +0600

    SQL Table Created

--- main.php
+++ main.php
@@ -14,7 +14,7 @@ $dbObject->connect();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Xemote Control Panel</title>
     <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
 </head>
