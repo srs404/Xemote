c6af73cd8877cc4fbb1535cf50e366104d77f62a
Samiur Rahman <srsami707@gmail.com>
2023-10-08 11:47:50 +0600

    database connection configuaration

--- assets/App/Model/Database.php
+++ assets/App/Model/Database.php
@@ -1,3 +1,21 @@
-class Database {
-    
-}
+<?php 
+
+    class Database {
+        private $credentials = [
+            'server'   => 'localhost',
+            'username' => 'root',
+            'password' => '',
+            'dbname'   => 'xemote'
+        ];
+
+        private function connection(){
+            try {
+                $conn = new PDO("mysql:host=" . $this -> credentials['server'] . ";dbname=" . $this -> credentials['dbname'], $this -> credentials['username'], $this -> credentials['password']);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }
+    }
